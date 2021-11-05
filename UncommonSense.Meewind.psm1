63e021911661375eebf7bda42ecc2342f37b88de
Jan Hoek <jhoek@uncommonsense.nl>
2021-11-05 11:21:08 +0100

    Repaired after site changes

--- UncommonSense.Meewind.psm1
+++ UncommonSense.Meewind.psm1
@@ -6,17 +6,17 @@ function Get-MeewindFundPrice
 
     $DutchCulture = [CultureInfo]::GetCultureInfo('nl-NL')
 
-    Invoke-WebRequest -Uri 'https://meewind.nl/fondsen'
-    | Select-Object -ExpandProperty Content
-    | pup '.fundblock:nth-of-type(1) json{}'
-    | jq '[.[] | { fund : .children[1].text, price : .children[2].children[2].children[1].children[1].text }]'
-    | ConvertFrom-Json
-    | ForEach-Object {
+    $Content = Invoke-WebRequest -Uri 'https://meewind.nl/fondsen' | Select-Object -ExpandProperty Content
+
+    $FundNames = $Content | pup '.fundblock .h4 text{}'
+    $FundPrices = $Content | pup '.fundblock .col-xs-6 .semibold:nth-child(2) text{}'
+
+    0..($FundNames.Count - 1) | ForEach-Object {
         [PSCustomObject]@{
             PSTypeName = 'UncommonSense.Meewind.FundPrice'
             Date       = Get-Date
-            Fund       = $_.fund
-            Price      = [decimal]::Parse(($_.price -replace '^€\s*', ''), $DutchCulture)
+            Fund       = $FundNames[$_]
+            Price      = [decimal]::Parse(($FundPrices[$_] -replace '^€\s*', ''), $DutchCulture)
         }
     }
 }
